2a7fc0d8c6eab360fc98c2d445e89f9dbc890134
Crystal Joyce <crystaljoyce@me.com>
2021-02-13 11:08:54 -0800

    added funcitons in seed to build and drop tables

--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,74 @@
-const { client } = require('./index');
+const { 
+    client, 
+    getAllUsers,
+} = require('./index');
 
+// async function testDB() {
+//     try{
+//         client.connect();
 
-async function testDB() {
+//         const users = await getAllUsers();
+//         console.log("users: ",users);
+//     } catch (error) {  
+//         console.error(error.message)
+//     } 
+// }
+
+// testDB(); 
+
+async function dropTables() {
     try{
-        client.connect();
-        const {rows} = await client.query(`SELECT * FROM users;`);
-        console.log("result: ",rows);
-    } catch (error) {  
-        console.error(error.message)
-    } finally{
-        client.end();
+        console.log('starting to drop tables ... ')
+        await client.query(`
+        DROP TABLE IF EXISTS users; 
+        `);
+        console.log('finsihed dropping tables ... ')
+    } catch (error) { 
+        console.error("error dropping tables!")
+        throw error; 
     }
 }
 
-testDB(); 
+async function createTables() {
+    try { 
+        console.log("starting to build tables ... ")
+        await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL
+        );
+        `);
+        console.log("finshed building tables ... ")
+    } catch (error) {
+        console.error("error building tables ")
+        throw error;
+    }
+}
+
+async function rebuildDB(){
+    try { 
+        client.connect();
+
+        await dropTables();
+        await createTables();
+    } catch (erorr) {
+        console.error(error.message)
+    } 
+}
+
+async function testDB(){
+    try{
+        console.log("starting to test database ... ");
+        const users = await getAllUsers();
+        console.log("getAllUsers; ",users);
+        console.log("finsihed database tests!")
+    } catch (error) {
+        console.error("There has been an error in the database test!")
+        throw error;
+    }
+}
+rebuildDB()
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
