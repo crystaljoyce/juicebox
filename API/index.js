e059af1ab6a5833af478950f59abda40cc1fd3c9
Crystal Joyce <crystaljoyce@me.com>
2021-02-17 20:06:10 -0800

    added code to users.js and index.js inside of API folder.

--- API/index.js
+++ API/index.js
@@ -1,25 +1,7 @@
-const PORT = 3000;
 const express = require('express');
-const server = express();
+const apiRouter = express.Router();
 
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
-});
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
 
-server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
-
-  app.use('/api', (req, res, next) => {
-    console.log("A request was made to /api");
-    next();
-  });
-  
-  app.get('/api', (req, res, next) => {
-    console.log("A get request was made to /api");
-    res.send({ message: "success" });
-  });
+module.exports = apiRouter;
---
+++ index.js
@@ -0,0 +1,27 @@
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+//   app.use('/api', (req, res, next) => {
+//     console.log("A request was made to /api");
+//     next();
+//   });
+  
+//   app.get('/api', (req, res, next) => {
+//     console.log("A get request was made to /api");
+//     res.send({ message: "success" });
+//   });
